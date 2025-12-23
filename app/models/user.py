da28f98072616c71765757b9628c8a788067b83b
utox39 <francescomoccaldi39@gmail.com>
2025-12-23 13:26:28 +0100

    feat(database): add user model

---
+++ app/models/user.py
@@ -0,0 +1,42 @@
+import datetime
+from enum import Enum
+
+
+class BloodType(Enum):
+    ANEG = 0
+    APOS = 1
+    BPOS = 2
+    BNEG = 3
+    OPOS = 4
+    ONEG = 5
+    ABPOS = 6
+    ABNEG = 7
+
+
+class User:
+    uuid: str
+    is_rescuer: bool
+    name: str
+    surname: str
+    birthday: datetime.date
+    blood_type: BloodType
+    # TODO: could be a new table
+    health_info_str: str
+
+    def __init__(
+        self,
+        uuid: str,
+        is_rescuer: bool,
+        name: str,
+        surname: str,
+        birthday: datetime.date,
+        blood_type: BloodType,
+        health_info_json: str,
+    ) -> None:
+        self.uuid = uuid
+        self.is_rescuer = is_rescuer
+        self.name = name
+        self.surname = surname
+        self.birthday = birthday
+        self.blood_type = blood_type
+        self.health_info_str = health_info_json
