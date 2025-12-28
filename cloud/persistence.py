bda5b6eab5d9057b89c3aee0c095587d0eb54657
utox39 <francescomoccaldi39@gmail.com>
2025-12-28 16:31:46 +0100

    feat(cloud-persist): add the save_encrypted_emergency method to store an encrypted emergency in the database

--- cloud/persistence.py
+++ cloud/persistence.py
@@ -1,13 +1,24 @@
-from common.models import *
-import sqlite3
+from common.models import db, enc_emergency
 
 
-def save_request():
+def save_encrypted_emergency(enc_emergency: enc_emergency.EncryptedEmergency):
+    """Persists an encrypted emergency request to the database.
+
+    This function stores the given `EncryptedEmergency` instance in the database.
+    The encrypted emergency is persisted until it is resolved or explicitly removed.
+
+    Args:
+        enc_emergency (enc_emergency.EncryptedEmergency): The EncryptedEmergency instance
+        to be persisted in the database.
+
+    Raises:
+        sqlite3.Error: If an error occurs while inserting the encrypted emergency into
+            the database.
     """
-    Persist a request to the DB until it's resolved or nullified
-    """
 
-    pass
+    dbm = db.DatabaseManager.get_instance()
+    dbm.insert_encrypted_emergency(enc_emergency)
+
 
 def delete_request():
     """
