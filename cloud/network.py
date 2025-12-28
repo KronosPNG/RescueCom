34844b8bc892cf8c39075218b09d4dee7bfc7528
vympel7 <vympel0@proton.me>
2025-12-28 12:57:05 +0100

    feat: rough cloud file structure

---
+++ cloud/network.py
@@ -0,0 +1,29 @@
+from common.models import *
+
+def multicast_request_to_rescuers():
+    """
+    Multicast an incoming request to available Rescuers
+    """
+
+    pass
+
+def assign_request_to_rescuer():
+    """
+    If more than one Rescuer accepts a request, choose one to assign it to
+    """
+
+    pass
+
+def sync_new_request():
+    """
+    Synchronise a new request to figure out how important and urgent it is relative to known unresolved requests
+    """
+
+    pass
+
+def establish_connection():
+    """
+    Establish a connection with a client by performing key exchange and begin communicating
+    """
+
+    pass
---
+++ cloud/persistence.py
@@ -0,0 +1,17 @@
+from common.models import *
+import sqlite3
+
+
+def save_request():
+    """
+    Persist a request to the DB until it's resolved or nullified
+    """
+
+    pass
+
+def delete_request():
+    """
+    Delete a request from the DB
+    """
+
+    pass
