4c79618b98775453bcf6dd0a50164ece21e3757c
vympel7 <vympel0@proton.me>
2025-12-29 11:32:46 +0100

    feat: broadcast emergency to Rescuers

--- cloud/app.py
+++ cloud/app.py
@@ -3,6 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# format UUID: (ip, enc_cipher, dec_cipher, nonce)
+CLIENTS = {}
+
+# subset of CLIENTS
+RESCUERS = {}
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5050)
--- cloud/network.py
+++ cloud/network.py
@@ -1,18 +1,48 @@
 from cryptography.hazmat.primitives.ciphers.aead import AESGCMSIV
 from common.models import *
 from common.services.crypto import *
-import os, requests
+import os, requests, uuid, app, struct, base64
 
 # TODO: do better
 CERTIFICATE_PATH = os.getenv("CERTIFICATE_PATH", "./certificates/cert.pem")
+DB_PATH = os.getenv("DB_PATH", "idk")
 
 
-def multicast_emergency_to_rescuers():
+def broadcast_emergency_to_rescuers(emergency: emergency.Emergency):
     """
     Multicast an emergency to available Rescuers
+
+    Args:
+        emergency (Emergency): emergency to send to Rescuers
     """
 
-    pass
+    # Utility function
+    def pack_str(s: str):
+        return struct.pack(f"<I@{}s".format(len(s)), len(s), s.encode())
+
+    for rescuer in app.RESCUERS:
+        ip, enc_cipher, dec_cipher, nonce = rescuer
+
+        encrypted_emergency = enc_emergency.EncryptedEmergency(
+                emergency.id,
+                emergency.user_uuid,
+                "", # unnecessary in this case
+                encrypt(enc_cipher, nonce,
+
+                        pack_str(emergency.position) +
+                        pack_str(emergency.address) +
+                        pack_str(emergency.city) +
+                        struct.pack("<I", emergency.street_number) +
+                        pack_str(emergency.place_description) +
+                        pack_str(emergency.photo_b64) +
+                        struct.pack("?", emergency.resolved) +
+                        pack_str(emergency.details_json),
+
+                        b""
+                    )
+                )
+
+        requests.post(ip + "/ask/vittorio", json={"encrypted_emergency": base64.b64encode(str(encrypted_emergency.to_db_tuple()))})
 
 def assign_emergency_to_rescuer():
     """
@@ -28,15 +58,14 @@ def sync_new_request():
 
     pass
 
-def establish_connection(client_ip: str, client_nonce: bytes) -> tuple[AESGCMSIV, AESGCMSIV]:
+def establish_connection(client_uuid: uuid.UUID, client_ip: str, client_nonce: bytes):
     """
     Establish a connection with a client by performing key exchange
 
     Args:
+        client_uuid (UUID): client's uuid
         client_ip (str): client's ip address
         client_nonce (bytes): client's nonce for the session
-    Returns:
-        Encryption and decryption ciphers to use while communicating with the client
     Raises:
         TypeError: if any argument is of the wrong type
     """
@@ -69,4 +98,9 @@ def establish_connection(client_ip: str, client_nonce: bytes) -> tuple[AESGCMSIV
 
     enc_cipher, dec_cipher = get_ciphers(key)
 
-    return enc_cipher, dec_cipher
+    app.CLIENTS[client_uuid] = (client_ip, enc_cipher, dec_cipher, client_nonce)
+
+    user = db.DatabaseManager.get_instance(DB_PATH).get_user_by_uuid(str(client_uuid))
+
+    if user is not None and user.is_rescuer:
+        app.RESCUERS[client_uuid] = (client_ip, enc_cipher, dec_cipher, client_nonce)
