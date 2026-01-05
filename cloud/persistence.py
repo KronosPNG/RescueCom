2e40fac31e0a8daabaca08cbfc1e9e382073efb2
utox39 <francescomoccaldi39@gmail.com>
2026-01-05 19:12:40 +0100

    feat(persistence): add methods for saving, updating and deleting the emergencies

--- cloud/persistence.py
+++ cloud/persistence.py
@@ -59,6 +59,78 @@ def delete_user(uuid: str) -> None:
     dbm.delete_user(uuid)
 
 
+def save_emergency(emergency: emergency.Emergency) -> None:
+    """
+    Persists an emergency to the database.
+
+    This function stores the given `Emergency` instance in the database by
+    delegating the operation to the `DatabaseManager`.
+    The emergency is persisted until it is resolved or explicitly removed.
+
+    Args:
+        emergency (emergency.Emergency): The Emergency instance to be
+            persisted in the database.
+
+    Raises:
+        sqlite3.Error: If an error occurs while inserting the emergency into
+            the database.
+    """
+
+    dbm = db.DatabaseManager.get_instance()
+    dbm.insert_emergency(emergency)
+
+
+def update_emergency(
+    user_uuid: str,
+    emergency_id: int,
+    emergency: emergency.Emergency,
+) -> None:
+    """
+    Updates an emergency record in the database.
+
+    This function updates the data of an emergency associated with the
+    given user UUID and emergency ID by delegating the operation to the
+    `DatabaseManager`.
+
+    Args:
+        user_uuid (str): The UUID of the user associated with the
+            emergency.
+        emergency_id (int): The unique identifier of the emergency to
+            update.
+        emergency (emergency.Emergency): An Emergency instance containing
+            the updated data.
+
+    Raises:
+        sqlite3.Error: If an error occurs while updating the emergency into
+            the database.
+    """
+
+    dbm = db.DatabaseManager.get_instance()
+    dbm.update_emergency(user_uuid, emergency_id, emergency)
+
+
+def delete_emergency(user_uuid: str, emergency_id: int):
+    """
+    Deletes an emergency from the database.
+
+    This function removes a specific emergency associated with the given
+    user UUID and emergency ID by delegating the operation to the
+    `DatabaseManager`.
+
+    Args:
+        user_uuid (str): The UUID of the user associated with the request.
+        emergency_id (int): The unique identifier of the emergency request
+        to delete.
+
+    Raises:
+        sqlite3.Error: If an error occurs while deleting the request from the
+            database.
+    """
+
+    dbm = db.DatabaseManager.get_instance()
+    dbm.delete_emergency(user_uuid, emergency_id)
+
+
 def save_encrypted_emergency(enc_emergency: enc_emergency.EncryptedEmergency) -> None:
     """Persists an encrypted emergency to the database.
 
