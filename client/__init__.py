7823b4bb5a6dc6cab9c389d84459803389b6a42a
vympel7 <vympel0@proton.me>
2026-01-08 13:10:56 +0100

    fix: avoid circular import

--- client/__init__.py
+++ client/__init__.py
@@ -4,7 +4,6 @@ import requests
 from typing import Optional
 from cryptography.hazmat.primitives.ciphers.aead import AESGCMSIV
 from flask import Flask
-from client import network
 
 
 DEC_CIPHER: Optional[AESGCMSIV] = None
@@ -12,9 +11,6 @@ ENC_CIPHER: Optional[AESGCMSIV] = None
 NONCE: Optional[bytes] = None
 CLOUD_NONCE: Optional[bytes] = None
 
-
 app = Flask(__name__)
 
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=1337)
+from . import network, routes
