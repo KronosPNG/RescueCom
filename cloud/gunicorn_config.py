dcfb373da9d1438fd538c42074096dff325b7519
vympel7 <vympel0@proton.me>
2026-01-12 11:32:28 +0100

    feat: cloud logging

---
+++ cloud/gunicorn_config.py
@@ -0,0 +1,13 @@
+import os
+import multiprocessing
+
+# Server settings
+bind = "0.0.0.0:8000"
+
+# Worker settings
+workers = 2 * multiprocessing.cpu_count() + 1
+
+# Logging
+accesslog = os.getenv("LOGS_DIR") + "/access.log"
+errorlog = os.getenv("LOGS_DIR") + "/error.log"
+loglevel = "debug"
