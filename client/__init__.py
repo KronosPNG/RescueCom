182d4310f4c2676278a9d062545d1fb573c6d133
utox39 <francescomoccaldi39@gmail.com>
2026-01-13 17:22:28 +0100

    feat(client): add ble listener

--- client/__init__.py
+++ client/__init__.py
@@ -4,6 +4,8 @@ import requests
 import uuid
 import datetime
 import time
+import sys
+import signal
 
 from common.services import crypto
 from common.models import db, user
@@ -13,6 +15,9 @@ from cryptography.hazmat.primitives.ciphers.aead import AESGCMSIV
 from flask import Flask
 from threading import Thread, Lock
 from dotenv import load_dotenv
+from common.models.emergency import Emergency
+from offline.service import BLEService
+from offline.payload import Payload
 
 load_dotenv(Path(__file__).parent / ".env")
 
@@ -32,11 +37,22 @@ CERTIFICATE_PATH = Path(os.getenv("CERTIFICATE_DIR", None)) / Path(
     os.getenv("CERTIFICATE_NAME", None)
 )
 
-db.DatabaseManager.get_instance(Path(os.getenv("DB_DIR", None)) / Path(os.getenv("DB_NAME", None)))
+ble_service = BLEService(
+    os.getenv("BLE_LOCALNAME", None), os.getenv("BLE_TARGET", None)
+)
+
+db.DatabaseManager.get_instance(
+    Path(os.getenv("DB_DIR", None)) / Path(os.getenv("DB_NAME", None))
+)
 
 CONNECTED: bool = False
 
 mutex = Lock()
+
+
+def shutdown_handler(sig, frame):
+    ble_service.stop()
+    sys.exit(0)
 
 
 def init_info():
@@ -107,11 +123,57 @@ def check_connection():
         time.sleep(60)
 
 
+def receive_bluetooth_payload():
+    ble_service.start()
+    while True:
+        payload: bytes | None = ble_service.receive_payload_nowait()
+        if payload:
+            # DB stuff
+            p: Payload = Payload.unpack_data(payload)
+            em: Emergency = Emergency(
+                emergency_id=p.emergency_id,
+                user_uuid=p.user_uuid,
+                severity=p.severity,
+                position=p.position,
+                emergency_type="Unknow",
+                description="Emergency from Blutooth",
+                created_at=datetime.datetime.now(),
+            )
+            dbm = db.DatabaseManager.get_instance()
+            try:
+                dbm.insert_emergency(em)
+            except Exception as e:
+                print(e)
+        time.sleep(0.5)
+
+
+def switch_to_bluetooth():
+    ble_thread: Thread | None = None
+    while True:
+        with mutex:
+            connected = CONNECTED
+
+        if not connected:
+            if ble_thread is None or not ble_thread.is_alive():
+                ble_thread = threading.Thread(
+                    target=receive_bluetooth_payload, daemon=True
+                )
+                ble_thread.start()
+
+        time.sleep(2)
+
+
+signal.signal(signal.SIGINT, shutdown_handler)
+signal.signal(signal.SIGTERM, shutdown_handler)
 init_info()
 init_certificate_and_skey()
 
 check_connection_thread: Thread = threading.Thread(target=check_connection, daemon=True)
 check_connection_thread.start()
+switch_to_bluetooth_thread: Thread = threading.Thread(
+    target=switch_to_bluetooth, daemon=True
+)
+switch_to_bluetooth_thread.start()
 
 app = Flask(__name__)
 
